df27a862683d1317ebd416f8203db6d75b9570cd
Wandir Pereira Filho <wandir@gmail.com>
2022-07-01 19:28:33 -0300

    Teste da validação de erro no pré-commit

--- src/components/Main/test.tsx
+++ src/components/Main/test.tsx
@@ -7,7 +7,7 @@ describe('<Main />', () => {
     const { container } = render(<Main />)
 
     expect(
-      screen.getByRole('heading', { name: /react avançado/i })
+      screen.getByRole('heading', { name: /react avançado 2/i })
     ).toBeInTheDocument()
 
     expect(container.firstChild).toMatchSnapshot()
