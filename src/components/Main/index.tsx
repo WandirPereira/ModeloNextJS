4e17661fe77c05d94475f71b41ec012583b6dac5
Wandir Pereira Filho <wandir@gmail.com>
2022-07-03 10:28:06 -0300

    Ajustando página principal

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,3 +1,19 @@
-const Main = () => <div>{<h1>React Avançado</h1>}</div>
+import * as S from './styles'
 
+const Main = () => (
+  <S.Wrapper>
+    <S.Logo
+      src="/img/logo.svg"
+      alt="Imagem de um átomo e React Avançado escrito ao lado"
+    />
+    <S.Title>React Avançado</S.Title>
+    <S.Description>
+      TypeScript, ReactJS, NextJS e Styled Components
+    </S.Description>
+    <S.Illustration
+      src="/img/hero-illustration.svg"
+      alt="Um desenvolvedor em frente a uma tela com código"
+    />
+  </S.Wrapper>
+)
 export default Main
