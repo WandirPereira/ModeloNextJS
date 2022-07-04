8441afa57b2c8e999471b8ad17f5f8d3ee7261c5
Wandir Pereira Filho <wandir@gmail.com>
2022-07-04 11:58:16 -0300

    Instalando e configurando o PWA

--- next.config.js
+++ next.config.js
@@ -1,6 +1,26 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true
-}
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true
+// }
 
-module.exports = nextConfig
+// module.exports = nextConfig
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const withPWA = require('next-pwa')
+const isProd = process.env.NODE_ENV === 'production'
+
+module.exports = withPWA({
+  swcMinify: true,
+  // experimental: {
+  //   // Enables the styled-components SWC transform
+  //   styledComponents: true
+  // },
+  compiler: {
+    // Enables the styled-components SWC transform
+    styledComponents: true
+  },
+  pwa: {
+    dest: 'public',
+    disable: !isProd
+  }
+})
